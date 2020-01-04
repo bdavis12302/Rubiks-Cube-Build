194fd3818830bd6b051fb08a11f5f7b8dbb99d09
Seth Harling <sethharling@gmail.com>
2020-01-03 22:54:16 -0700

    Update rubiks-cube-solver.py
    created getSide() function
    
    created variable for a solved version of each side.

--- rubiks-cube-solver.py
+++ rubiks-cube-solver.py
@@ -3,6 +3,27 @@
 #        white red yellow orange blue green
 cube = [ [  ], [  ], [  ], [  ], [  ], [  ] ]
 
+solvedWhite = ['w1', 'w2', 'w3', 'w4', 'w5', 'w6', 'w7', 'w8']
+solvedRed = ['r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7', 'r8']
+solvedYellow = ['y1', 'y2', 'y3', 'y4', 'y5', 'y6', 'y7', 'y8']
+solvedOrange = ['o1', 'o2', 'o3', 'o4', 'o5', 'o6', 'o7', 'o8']
+solvedBlue = ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8']
+solvedGreen = ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8']
+
+def getSide(side):
+    if side == w:
+        return cube[0]
+    elif side == r:
+        return cube[1]
+    elif side == y:
+        return cube[2]
+    elif side == o:
+        return cube[3]
+    elif side == b:
+        return cube[4]
+    elif side == g:
+        return cube[5]
+
 def solve(cube):
     solved = False
     while solved == False:
