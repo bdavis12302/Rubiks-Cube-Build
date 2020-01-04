6a41ddce67c0b37283a4782031f8ead592d23a87
Seth Harling <sethharling@gmail.com>
2020-01-03 22:38:27 -0700

    Update rubiks-cube-solver.py
    created what a solved cube should look like

--- rubiks-cube-solver.py
+++ rubiks-cube-solver.py
@@ -4,4 +4,17 @@
 cube = [ [  ], [  ], [  ], [  ], [  ], [  ] ]
 
 def solve(cube):
-    pass
+    solved = False
+    while solved == False:
+        if cube == [
+            ['w1', 'w2', 'w3', 'w4', 'w5', 'w6', 'w7', 'w8'],
+            ['r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7', 'r8'],
+            ['y1', 'y2', 'y3', 'y4', 'y5', 'y6', 'y7', 'y8'],
+            ['o1', 'o2', 'o3', 'o4', 'o5', 'o6', 'o7', 'o8'],
+            ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8'],
+            ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8']
+        ]:
+            solved = True
+        else:
+            solved = False
+            # ^ temporary
