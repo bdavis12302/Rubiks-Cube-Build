f0f738f116a98f7a7cefbf42d4c991438115b49e
Seth Harling <sethharling@gmail.com>
2020-01-03 21:33:00 -0700

    Create rubiks-cube-solver.py
    rubiks-cube-solver.py

---
+++ rubiks-cube-solver.py
@@ -0,0 +1,6 @@
+#rubiks cube solver
+#color code: white=1, red=2, yellow=3, orange=4, blue=5, green=6
+#           white           red             yellow          orange          blue            green
+cube = [ [ [], [], [] ], [ [], [], [] ], [ [], [], [] ], [ [], [], [] ], [ [], [], [] ], [ [], [], [] ] ]
+
+def solve(cube):
